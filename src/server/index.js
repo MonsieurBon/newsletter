c0158fd1e7e17b153b808793cb1f2ee7b9f991d4
Fabian Gut <fabian.gut@ethy.ch>
2019-06-21 06:41:47 +0200

    Setup development environment

---
+++ src/server/index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/api', (req, res) => res.send('Hello World'))
+
+app.listen(port, () => console.log(`App listening on port ${port}`))
